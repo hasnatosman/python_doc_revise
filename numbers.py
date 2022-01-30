9d625b403c9e572f2dc416fbf499669d19cd844f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2022-01-30 22:19:52 +0600

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,27 @@
+#declaring variables...........................
+num1 = 8
+num2 = 3
+
+
+#operations.......................................
+addition = num1 + num2
+subtractions = num1 - num2
+multiplication = num1 * num2
+classical_division = num1 / num2
+floor_division = num1 // num2
+reminder_division = num1 % num2
+power = num1 ** num2
+
+
+
+
+#printing output.....................................
+print(" >>>>>>>>>>>>>>>>>> Here are the outputs !!!!!!!")
+print()
+print("Total is: ", addition)
+print("Difference is: ", subtractions)
+print("Multiplication is : ", multiplication)
+print("Classical Division is : ", classical_division)
+print("Floor Division is : ", floor_division)
+print("Reminder Division is : ", reminder_division)
+print("Power is : ", power)
