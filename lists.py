c7be0b1447a078a0566ec1131fa6f8e1bec1d00c
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-02-05 11:14:39 +0600

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,31 @@
+#variables .................................................
+
+
+numb = [2, 3, 4, 5]
+char = ["A","B", "C", "D"]
+
+
+
+
+
+#printing outputs...........................................
+
+print(numb)
+print(char)
+
+
+#indexing return.........................
+print(numb[1])
+print(char[0])
+
+print(numb[-3: ]) #slicing...............
+
+#mutable ................................
+numb[0]=1
+print(numb)
+
+
+
+# list can be mutable....................
+concate = numb + char
+print(concate)
